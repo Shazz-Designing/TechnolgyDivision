abe92ba15589f78cf5b023ed0d80b6709240d6ef
Sharon Apot <sharonapot@gmail.com>
2022-08-14 12:43:15 +0300

    Add user input fields

--- src/main/java/org/safaricom/Input.java
+++ src/main/java/org/safaricom/Input.java
@@ -1,5 +1,18 @@
 package org.safaricom;
 
-public class Input {
+import java.util.Scanner;
 
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter new Employee Name: ");
+        String nEmpName = input.next();
+        System.out.println("Enter new Employee Department: ");
+        String nEmpDept = input.next();
+        System.out.println("Enter new Employee Role: ");
+        String nEmpRole = input.next();
+        System.out.println("Enter new Employee Responsibilities: ");
+        String nEmpResp = input.next();
+        System.out.println("Record Saved");
+    }
 }
